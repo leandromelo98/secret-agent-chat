5e30daaee1d20b0424f464fd8085f90db11014ec
leandro melo <leandro melo>
2016-03-16 15:38:48 +0900

    finished first step

---
+++ secret-agent-chat.py
@@ -0,0 +1,9 @@
+from random import randint
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+
+def generate_otp(sheets, length):
+    for sheet in range(sheets):
+        with open("otp" + str(sheet) + ".txt", "w") as f:
+            for i in range(length):
+                f.write(str(randint(0, 26))+"\n")
