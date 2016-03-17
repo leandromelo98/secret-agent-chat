5d972855d9c70777fe41606f3d1f3fd0534ae94e
leandro melo <leandro melo>
2016-03-17 15:43:09 +0900

    fixed errors

--- secret-agent-chat.py
+++ secret-agent-chat.py
@@ -8,6 +8,7 @@ def generate_otp(sheets, length):
             for i in range(length):
                 f.write(str(randint(0, 26))+"\n")
 
+
 def load_sheet(filename):
     with open(filename, "r") as f:
 
@@ -15,18 +16,23 @@ def load_sheet(filename):
 
     return contents
 
+
 def get_plain_text():
     plain_text = input('Please type your message ')
+
     return plain_text.lower()
+
 
 def load_file(filename):
     with open(filename, "r") as f:
         contents = f.read()
     return contents
 
+
 def save_file(filename, data):
     with open(filename, 'w') as f:
         f.write(data)
+
 
 def encrypt(plaintext, sheet):
     ciphertext = ''
@@ -38,6 +44,7 @@ def encrypt(plaintext, sheet):
             ciphertext += ALPHABET[encrypted]
     return ciphertext
 
+
 def decrypt(ciphertext, sheet):
     plaintext = ''
     for position, character in enumerate(ciphertext):
@@ -46,7 +53,9 @@ def decrypt(ciphertext, sheet):
         else:
             decrypted = (ALPHABET.index(character) - int(sheet[position])) % 26
             plaintext += ALPHABET[decrypted]
+
     return plaintext
+
 
 def menu():
     choices = ['1', '2', '3', '4']
@@ -60,20 +69,26 @@ def menu():
             print('4. Quit the program')
             choice = input('Please type 1, 2, 3 or 4 and press Enter ')
             if choice == '1':
-                sheets = int(input('How many one-time pads would you like to generate? '))
-                length = int(input('What will be your maximum message length? '))
+                sheets = int(input('''How many one-time pads would
+                                    you like to generate? '''))
+                length = int(input('''What will be your maximum message
+                                   length? '''))
                 generate_otp(sheets, length)
             elif choice == '2':
-                filename = input('Type in the filename of the OTP you want to use ')
+                filename = input('''Type in the filename of the OTP you want
+                                 to use ''')
                 sheet = load_sheet(filename)
                 plaintext = get_plaintext()
                 ciphertext = encrypt(plaintext, sheet)
-                filename = input('What will be the name of the encrypted file? ')
+                filename = input('''What will be the name of the
+                                    encrypted file? ''')
                 save_file(filename, ciphertext)
             elif choice == '3':
-                filename = input('Type in the filename of the OTP you want to use ')
+                filename = input('''Type in the filename of the OTP you want
+                                    to use ''')
                 sheet = load_sheet(filename)
-                filename = input('Type in the name of the file to be decrypted ')
+                filename = input('''Type in the name of the file to be
+                                    decrypted ''')
                 ciphertext = load_file(filename)
                 plaintext = decrypt(ciphertext, sheet)
                 print('The message reads:')
