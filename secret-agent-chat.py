54210d637cfcf460d463bdc101eeba6a745d369a
leandro melo <leandro melo>
2016-03-17 15:28:51 +0900

    finished step 7

--- secret-agent-chat.py
+++ secret-agent-chat.py
@@ -47,3 +47,40 @@ def decrypt(ciphertext, sheet):
             decrypted = (ALPHABET.index(character) - int(sheet[position])) % 26
             plaintext += ALPHABET[decrypted]
     return plaintext
+
+def menu():
+    choices = ['1', '2', '3', '4']
+    choice = '0'
+    while True:
+        while choice not in choices:
+            print('What would you like to do?')
+            print('1. Generate one-time pads')
+            print('2. Encrypt a message')
+            print('3. Decrypt a message')
+            print('4. Quit the program')
+            choice = input('Please type 1, 2, 3 or 4 and press Enter ')
+            if choice == '1':
+                sheets = int(input('How many one-time pads would you like to generate? '))
+                length = int(input('What will be your maximum message length? '))
+                generate_otp(sheets, length)
+            elif choice == '2':
+                filename = input('Type in the filename of the OTP you want to use ')
+                sheet = load_sheet(filename)
+                plaintext = get_plaintext()
+                ciphertext = encrypt(plaintext, sheet)
+                filename = input('What will be the name of the encrypted file? ')
+                save_file(filename, ciphertext)
+            elif choice == '3':
+                filename = input('Type in the filename of the OTP you want to use ')
+                sheet = load_sheet(filename)
+                filename = input('Type in the name of the file to be decrypted ')
+                ciphertext = load_file(filename)
+                plaintext = decrypt(ciphertext, sheet)
+                print('The message reads:')
+                print('')
+                print(plaintext)
+            elif choice == '4':
+                exit()
+            choice = '0'
+
+menu()
