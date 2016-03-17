0c56b20aec95a49970c943019ae35a241a860b58
leandro melo <leandro melo>
2016-03-17 15:20:51 +0900

    finished part 4

--- secret-agent-chat.py
+++ secret-agent-chat.py
@@ -19,3 +19,12 @@ def get_plain_text():
     plain_text = input('Please type your message ')
     return plain_text.lower()
 
+def load_file(filename):
+    with open(filename, "r") as f:
+        contents = f.read()
+    return contents
+
+def save_file(filename, data):
+    with open(filename, 'w') as f:
+        f.write(data)
+
